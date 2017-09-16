499628bccd64d321615a8e5ec7d427714a0f4759
Axel <axelfuh@gmail.com>
2017-09-16 22:17:00 +0200

    🐶

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,14 +4,8 @@ import { apiKey } from './utils'
 const url = "https://newsapi.org/v1/articles?source=the-next-web"
 
 
-/*
-top:	Requests a list of the source's headlines sorted in the order they appear on its homepage.
-latest:	Requests a list of the source's headlines sorted in chronological order, newest first.
-popular:	Requests a list of the source's current most popular or currently trending headlines.
-*/
-
-export function getNews(sort) {
-  return axios.get(`${url}&sortBy=${sort}&apiKey=${apiKey}`)
+export function getNews() {
+  return axios.get(`${url}&sortBy=latest&apiKey=${apiKey}`)
     .then(res => res.data.articles)
     .catch(err => console.log(err))
 }
